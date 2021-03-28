5a689aa5ca9316e542bde67a1714ba038afa3c42
Søren Hougaard Mulvad <shmulvad@gmail.com>
2021-03-28 17:35:18 +0800

    Update tests for featurizer after fixing bug

--- src/tests/test_featurizer.py
+++ src/tests/test_featurizer.py
@@ -45,9 +45,10 @@ class TestUrlRawSplitter:
 
     def test_feature_vec(self):
         feat = create_feat(1, 1, 1, 1)
-        # is_https, num_main_domain_words, num_sub_domains is_www,
-        # is_www_weird, path_len, domain_end_verdict
-        # domains_num_len, path_num_len, args_num_len, total_num_len
+        # is_https, num_main_domain_words, num_sub_domains,
+        # is_www, is_www_weird, path_len, domain_end_verdict,
+        # sub_domains_num_digits, path_num_digits, args_num_digits,
+        # total_num_digits
         vec, _ = feat.featurize('http://test.com')
         assert np.allclose(vec, np.array([0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]))
 
@@ -55,13 +56,16 @@ class TestUrlRawSplitter:
         assert np.allclose(vec, np.array([1, 3, 0, 0, 0, 0, -1, 0, 0, 0, 0]))
 
         vec, _ = feat.featurize('https://www.sub1.sub2.test.unknown')
-        assert np.allclose(vec, np.array([1, 1, 3, 1, 0, 0, 0, 0, 0, 0, 0]))
+        assert np.allclose(vec, np.array([1, 1, 3, 1, 0, 0, 0, 2, 0, 0, 2]))
 
         vec, _ = feat.featurize('https://wwws.test.com/some/long/long/path')
         assert np.allclose(vec, np.array([1, 1, 1, 0, 1, 4, 1, 0, 0, 0, 0]))
 
         vec, _ = feat.featurize('http://www2.117.ne.jp/~mb1996ax/enadc.html')
-        assert np.allclose(vec, np.array([0, 1, 2, 0, 1, 7, 0, 3, 4, 0, 7]))
+        assert np.allclose(vec, np.array([0, 1, 2, 0, 1, 7, 0, 4, 4, 0, 8]))
+
+        vec, _ = feat.featurize('http://www.t.com/path?arg1=val11;arg2=val22')
+        assert np.allclose(vec, np.array([0, 1, 1, 1, 0, 1, 1, 0, 0, 6, 6]))
 
     def test_word_embed_matrix_size(self):
         feat = create_feat(1, 1, 1, 1)
