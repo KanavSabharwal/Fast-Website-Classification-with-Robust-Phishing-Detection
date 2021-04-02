f98e2af7313ecc9fb2a2c5a8c0ee5d298124fc4a
Søren Hougaard Mulvad <shmulvad@gmail.com>
2021-04-02 16:20:45 +0800

    Update pickle_data.py

--- src/pickle_data.py
+++ src/pickle_data.py
@@ -1,23 +1,35 @@
 import pickle
 
-from read_data import read_all_datasets
+from read_data import read_all_datasets, read_phishing_extra
+
+
+def pickle_main_datasets():
+    '''
+    Reads the main three datasets and pickles a dictionary of shuffled
+    DataFrames of them
+    '''
+    dmoz, phishing, ilp = read_all_datasets(use_sample=False)
+    data = {
+        'dmoz': dmoz.sample(frac=1),
+        'phishing': phishing.sample(frac=1),
+        'ilp': ilp.sample(frac=1)
+    }
+    with open('datasets.pkl', 'wb') as f:
+        pickle.dump(data, f)
+
+
+def pickle_extra_phishing():
+    '''
+    Reads the extra phishing datasets and pickles a shuffled DataFrame of it
+    '''
+    phishing_extra = read_phishing_extra().sample(frac=1)
+    with open('phishing_extra.pkl', 'wb') as f:
+        pickle.dump(phishing_extra, f)
 
 
 def main():
-    # Read and shuffle data
-    dmoz, phishing, ilp = read_all_datasets(use_sample=False)
-    dmoz = dmoz.sample(frac=1)
-    phishing = phishing.sample(frac=1)
-    ilp = ilp.sample(frac=1)
-
-    data = {
-        'dmoz': dmoz,
-        'phishing': phishing,
-        'ilp': ilp
-    }
-
-    with open('datasets.pkl', 'wb') as f:
-        pickle.dump(data, f)
+    pickle_main_datasets()
+    pickle_extra_phishing()
 
 
 if __name__ == '__main__':
