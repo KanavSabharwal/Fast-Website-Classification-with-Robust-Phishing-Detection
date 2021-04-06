57a3429279232dc35c86632a4eada599bce91b7a
Søren Hougaard Mulvad <shmulvad@gmail.com>
2021-04-06 22:13:35 +0800

    Refactor of self_trained_embeddings

--- src/self_trained_embeddings.py
+++ src/self_trained_embeddings.py
@@ -1,11 +1,10 @@
-import pandas as pd
-from os import path
 import re
+from typing import List
+
 from gensim.models import Word2Vec
 
-from url_tokenizer import flatten_url_data,url_tokenizer
+from url_tokenizer import flatten_url_data, url_tokenizer
 from read_data import read_dmoz, read_ilp
-from util import word_splitter
 
 '''
 Examples on how to get the self-trained embedding model.
@@ -14,26 +13,31 @@ embedding_dmoz = get_embedding(dataset="dmoz", use_sample=True, min_count=2)
 embedding_ilp = get_embedding(dataset="ilp", use_sample=False, min_count=2)
 '''
 
+DMOZ, ILP = 'DMOZ', 'ILP'
 
-def url_ipl_splitter(url: str,use_sample:bool=False):
+
+def url_ilp_splitter(url: str, use_sample: bool = False):
     '''
-    generating the urls in ilp dataset into a tuple of (protocol, domains, path, args).
-    args:
+    Transforms the urls in ilp dataset into a tuple of
+    (protocol, domains, path, args).
+
+    Args:
         url (str): Full url of webpage.
-    return:
-        a tuple of (protocol, domains, path, args).
-    
-    remaining issue: only works on webkb, cannot process the webkb_sample dataset.
+
+    Returns:
+        A tuple of (protocol, domains, path, args).
     '''
-    
-    # RegEx partly based on http_//cs.cornell.edu/Info/Courses/Current/CS415/CS414.html
+    # TODO: Only works on webkb, cannot process the webkb_sample dataset.
+
+    # RegEx partly based on
+    # http_//cs.cornell.edu/Info/Courses/Current/CS415/CS414.html
     url_regex = re.compile(r'''
         (https?)\_\/\/                                   # http s
-        ([-a-zA-Z0-9@:%._\+~#=]+\.[a-zA-Z0-9()]{1,12})  # domains
+        ([-a-zA-Z0-9@:%._\+~#=]+\.[a-zA-Z0-9()]{1,12})   # domains
         \b
-        ([-a-zA-Z0-9()@:%_\+;.~#&//=]*)                 # path
+        ([-a-zA-Z0-9()@:%_\+;.~#&//=]*)                  # path
         \??
-        ([-a-zA-Z0-9()@:%_\+;.~#&//=?]*)                # args
+        ([-a-zA-Z0-9()@:%_\+;.~#&//=?]*)                 # args
     ''', re.DOTALL | re.VERBOSE)
     match = url_regex.match(url.lower())
     assert match, f'Error matching url: {url}'
@@ -41,42 +45,36 @@ def url_ipl_splitter(url: str,use_sample:bool=False):
     return raw_values
 
 
-
-def dmoz_sentence_handler(use_sample):
+def dmoz_sentence_handler(use_sample: bool = False):
     '''
-    clean and extract urls from dmoz dataset into the input form.
-    
-    return:
-        sentence (list[list]):a list of sentences, where each sentence is a list of words from url dataset.
-    '''
+    Cleans and extracts URLs from DMOZ dataset into the input form.
 
+    Returns:
+        sentences (List[List[str]]): A list of sentences, where each sentence
+            is a list of words from URL dataset.
+    '''
     dmoz = read_dmoz(use_sample)
-    urls = list(dmoz['url'].to_numpy())
-
-    sentence_for_train = []
-    for url in urls:
-        url_data = url_tokenizer(url)
-        words = flatten_url_data(url_data)
-        sentence_for_train.append(words)
-
-    return sentence_for_train
+    urls = dmoz['url'].to_numpy()
+    sentences_for_train = [flatten_url_data(url_tokenizer(url))
+                           for url in urls]
+    return sentences_for_train
 
 
-
-def ilp_sentence_handler(use_sample):
+def ilp_sentence_handler(use_sample: bool = False):
     '''
-    clean and extract urls from ilp dataset into the input form.
+    Clean and extracts urls from ILP dataset into the input form.
 
-    return:
-        sentence (list[list]):a list of sentences, where each sentence is a list of words from url dataset.
+    Returns:
+        sentences (List[List[str]]): A list of sentences, where each sentence
+            is a list of words from URL dataset.
     '''
     ilp = read_ilp(use_sample)
-    urls = list(ilp['url'].to_numpy())
-    
-    sentence_for_train = []
+    urls = ilp['url'].to_numpy()
+
+    sentences_for_train = []
     for url in urls:
         url_data = []
-        protocol, domains, path, args = url_ipl_splitter(url)
+        protocol, domains, path, args = url_ilp_splitter(url)
         if protocol is not None:
             url_data.append(protocol)
         if domains is not None:
@@ -86,49 +84,46 @@ def ilp_sentence_handler(use_sample):
             path = re.findall(r"[\w']+", path)
             url_data += path
 
-        assert args == ''
-        
-        sentence_for_train.append(url_data)
+        sentences_for_train.append(url_data)
 
-    return sentence_for_train
+    return sentences_for_train
 
 
-
-def train_embedding_Word2Vec(sentence):
+def train_embedding_Word2Vec(sentences: List[List[str]]):
     '''
-    train the embedding Word2Vec model using gensim.
+    Trains the Word2Vec model using gensim and returns the embedding.
+
     Args:
-        sentence (list[list]):a list of sentences, where each sentence is a list of words from url dataset.
-        e.g. [['http', 'cs-www', 'bu', 'edu', 'students', 'grads', 'ghuang', 'cs111', 'class', 'html'], 
-        ['http', 'cs-www', 'bu', 'edu', 'students', 'grads', 'rgaimari', 'cs101', 'f96', 'cs101', 'html'],...]
+        sentences (List[List[str]]): A list of sentences, where each sentence
+            is a list of words from URL dataset.
+            e.g. [['http', 'cs-www', 'bu'], ['http', 'www', 'bu', 'edu'], ...]
+
     Returns:
-        the trained embedding model. 
-
+        The trained embedding model.
     '''
-    embedding = Word2Vec(sentence,min_count=2)
+    embedding = Word2Vec(sentences, min_count=2)
     return embedding
 
 
-def get_embedding(dataset,use_sample:bool=False,min_count:int=2):
+def get_embedding(dataset: str, use_sample: bool = False, min_count: int = 2):
     '''
-    generate the embedding model for the choosen dataset.
-    args:
-        dataset (str): dataset's name. "dmoz" or "ilp"
-        use_sample: if using the sample dataset.
-        min_count (int): Ignore words that appear less than this when training the embedding model.
+    Generate the embedding model for the choosen dataset.
 
-    return: the embedding model.
+    Args:
+        dataset (str): Dataset's name. "dmoz" or "ilp"
+        use_sample (bool): Whether to use sample dataset.
+        min_count (int): Ignore words that appear less than this when training
+            the embedding model.
+
+    Returns:
+        The embedding model.
     '''
-
-    sentence = None
-    if dataset == "dmoz":
-        sentence = dmoz_sentence_handler(use_sample)
-    elif dataset == "ilp":
-        sentence = ilp_sentence_handler(use_sample)
-    
-    embedding = train_embedding_Word2Vec(sentence)
-
+    sentence_handler_func = {
+        DMOZ: dmoz_sentence_handler,
+        ILP: ilp_sentence_handler
+    }
+    assert dataset in sentence_handler_func, \
+        f'Only {list(sentence_handler_func.keys())} supported.'
+    sentences = sentence_handler_func[dataset](use_sample)
+    embedding = train_embedding_Word2Vec(sentences)
     return embedding
-    
-
-
